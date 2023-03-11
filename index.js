1420d5a7ab127c295184778b25bcce06b662506a
BryanRonad <bryanronad@gmail.com>
2023-03-11 14:11:25 +0530

    feat(session): ws server creation, token verification, connection check

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import cors from "cors";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import { logger } from "./middlewares/logger.middleware.js";
 import cookieParser from "cookie-parser";
+import { connections } from "./controllers/session.controllers.js";
 dotenv.config();
 
 //Initialization
 const app = express();
+
 //Server load handler
 app.use(function (req, res, next) {
   if (toobusy()) {
@@ -33,13 +35,26 @@ app.use(cookieParser());
 
 //Routers
 import homeRouter from "./routes/home.routes.js";
+import sessionRouter from "./routes/session.routes.js";
 
 //Parent routes
 app.use("/api/", homeRouter);
+app.use("/api/session", sessionRouter);
 
 //Middlewares (Post routes)
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 5000, () => {
+process.on("SIGINT", () => {
+  console.log("Closing all connections");
+  connections.forEach((ws, token) => {
+    console.log("Closing connetion " + token);
+    ws.close();
+  });
+  console.log("Websocket connections closed, exiting...");
+  process.exit();
+});
+
+const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`Sandbox listening port ${process.env.PORT || 5000}`);
 });
+app.set("server", server);
